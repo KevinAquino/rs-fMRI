2b55a97500da5f88a9ed9b9e8b2344c30380c2eb
Kevin Aquino <kevin.aquino@monash.edu>
2018-04-15 22:09:15 +1000

    added flags to work with new project

--- prepro/MASSIVE_Job.sh
+++ prepro/MASSIVE_Job.sh
@@ -13,15 +13,15 @@
 #SBATCH --array=1-100%50
 
 # Assign input args
-WhichMASSIVE=$1
-WhichProject=$2
-WhichSessScan=$3
+WhichProject=$1
+WhichSessScan=$2
 
 case $WhichProject in
 	OCDPG) SUBJECT_LIST="/gpfs/M2Home/projects/Monash076/Linden/Sublists/OCDPG.txt" ;;
 	UCLA) SUBJECT_LIST="/gpfs/M2Home/projects/Monash076/Linden/Sublists/UCLA.txt" ;;
 	NYU_2) SUBJECT_LIST="/gpfs/M2Home/projects/Monash076/Linden/Sublists/NYU_2.txt" ;;
 	GoC) SUBJECT_LIST="/projects/kg98/kristina/code/sublists/GoC.txt" ;;
+	UCLA_native_MNI) SUBJECT_LIST="/home/kaqu0001/kg98_scratch/kevo/GSR_data/UCLA_physio_subjects.txt" ;;
 esac
 
 # MASSIVE modules
@@ -30,7 +30,7 @@ module load fsl/5.0.9
 module load python/2.7.11-gcc
 module load ants/1.9.v4
 module load afni/16.2.16
-module load spm8/matlab2014a.r5236
+module load spm12/matlab2015b.r6685 
 
 subject=$(sed -n "${SLURM_ARRAY_TASK_ID}p" ${SUBJECT_LIST})
 echo -e "\t\t\t --------------------------- "
@@ -38,7 +38,7 @@ echo -e "\t\t\t ----- ${SLURM_ARRAY_TASK_ID} ${subject} ----- "
 echo -e "\t\t\t --------------------------- \n"
 
 # Run pipeline
-cd /gpfs/M2Home/projects/Monash076/Linden/scripts/rs-fMRI/prepro/
+cd /home/kaqu0001/kg98/kevo/Scripts/rs-fMRI/prepro
 
 smoothing=before
 discard=1
@@ -47,4 +47,4 @@ despike=1
 detr=1
 intnorm=1
 
-matlab -nodisplay -r "run_prepro('${WhichMASSIVE}','${WhichProject}','${WhichSessScan}','${subject}','$smoothing',$discard,$slicetime,$despike,$detr,$intnorm); exit"
+matlab -nodisplay -r "run_prepro('${WhichProject}','${WhichSessScan}','${subject}','$smoothing',$discard,$slicetime,$despike,$detr,$intnorm); exit"
